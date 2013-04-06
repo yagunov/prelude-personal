394fab00056521adcd12f7dea180648f07bee6f6
Andrey Yagunov <yagunov86@gmail.com>
2013-04-07 02:33:02 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(safe-local-variable-values (quote ((time-stamp-time-zone . UTC)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(secondary-selection ((t (:background "black"))))
- '(which-func ((t (:background "black" :foreground "green3")))))
+)
